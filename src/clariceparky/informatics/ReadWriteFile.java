2e0128a3414718942ca78a7c20b5d4855c6fae0d
claricep <clariceparky24@gmail.com>
2021-06-18 16:28:01 -0700

    my first test

---
+++ src/clariceparky/informatics/ReadWriteFile.java
@@ -0,0 +1,16 @@
+package clariceparky.informatics;
+
+public class ReadWriteFile {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        testIt();
+
+
+    }
+
+    public static void testIt() {
+
+        System.out.println("run test it method");
+    }
+}
