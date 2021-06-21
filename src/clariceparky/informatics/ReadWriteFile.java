24501089dc4e11d32dcf3944eabe7c59e73d84a2
claricep <clariceparky24@gmail.com>
2021-06-21 11:21:42 -0700

    my first test

--- src/clariceparky/informatics/ReadWriteFile.java
+++ src/clariceparky/informatics/ReadWriteFile.java
@@ -1,16 +1,30 @@
 package clariceparky.informatics;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+
 public class ReadWriteFile {
-    public static void main(String[] args) {
-        System.out.println("hello world !!!");
+    public static void main(String[] args) throws Exception {
+//google java type: integer, double, String, etc.
+        String fileName = "/Users/cpark/Downloads/ms2.txt";
+        String output = "/Users/cpark/Downloads/ms2_out.txt";
 
-        testIt();
+        BufferedOutputStream bo =
+                new BufferedOutputStream(new FileOutputStream(output));
+
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String line = br.readLine();
+        while(line != null) {
+            System.out.println(line);
+            line = br.readLine();
+            //br.write(string);
+        }
+
+        br.close();
+        bo.close();
 
 
-    }
-
-    public static void testIt() {
-
-        System.out.println("run test it method");
     }
 }
